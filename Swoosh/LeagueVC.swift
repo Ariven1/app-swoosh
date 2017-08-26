878505ec859f1efc568138898a920c8cc3a18b29
Ariven <ariven@Arivens-Mac.local>
2017-08-26 15:12:17 +0530

    lesson09 - Creating Model and Passing Data

--- Swoosh/LeagueVC.swift
+++ Swoosh/LeagueVC.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var btnNextOutlet: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +28,27 @@ class LeagueVC: UIViewController {
     @IBAction func btnNext(_ sender: Any) {
         
         performSegue(withIdentifier: "LeagueVCIdentifier", sender: self)
+    }
+    
+    @IBAction func btnMens(_ sender: Any) {
+        
+        selectLeague(myLeague: "mens")
+    }
+    
+    @IBAction func btnWomens(_ sender: Any) {
+        
+        selectLeague(myLeague: "womens")
+    }
+    
+    @IBAction func btnCoed(_ sender: Any) {
+        
+        selectLeague(myLeague: "coed")
+    }
+    
+    func selectLeague(myLeague: String){
+    
+        player.desiredLeague = myLeague
+        btnNextOutlet.isEnabled = true
     }
     
     
---
+++ Swoosh/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Ariven on 26/08/17.
+//  Copyright © 2017 Ariven. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
